be2bc214dc55e071abe2533980afc4349819908a
rakesh <rakeshhotker@gmail.com>
2023-02-21 23:20:50 +0530

    fixed  Show tileset age #56

--- src/Controls/Directions/Summary.jsx
+++ src/Controls/Directions/Summary.jsx
@@ -8,6 +8,20 @@ import { Icon, Checkbox, Popup } from 'semantic-ui-react'
 import { showProvider } from '../../actions/directionsActions'
 
 class Summary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { tileset_last_modified: 0 }
+  }
+  componentDidMount() {
+    this.getTileModifiedData()
+  }
+  async getTileModifiedData() {
+    const res = await (
+      await fetch('https://valhalla1.openstreetmap.de/status')
+    ).json()
+    console.log(res)
+    this.setState({ tileset_last_modified: res.tileset_last_modified })
+  }
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     results: PropTypes.object,
@@ -28,18 +42,30 @@ class Summary extends React.Component {
     const minutes = date.getMinutes() > 0 ? date.getMinutes() + 'min' : ''
     return days + hours + minutes
   }
-
+  tileLastModifiedTime = (durationInSeconds) => {
+    const date = new Date(durationInSeconds * 1000)
+    const days = new Date().getDate() - date.getDate() + 'd'
+    const hours = new Date().getHours() - date.getHours() + 'h'
+    return days + ' ' + hours
+  }
   render() {
     const { provider, results, inclineDeclineTotal } = this.props
 
     const summary = R.path([provider, 'data', 'trip', 'summary'], results)
-
+    console.log(summary.time)
     return (
       <React.Fragment>
         {summary ? (
           <React.Fragment>
             <div className="flex mb1">
-              <span className="b">Directions</span>
+              <span className="b">Directions</span>&nbsp;&nbsp;
+              <span className="b">
+                TileSet_Last_Modified:&nbsp;
+                {`${this.tileLastModifiedTime(
+                  this.state.tileset_last_modified
+                )}`}
+                &nbsp;ago
+              </span>
             </div>
             <div className={'flex justify-between pb2 pointer'}>
               <div
