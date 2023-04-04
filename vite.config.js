d567874c1188f8e557b55700f02c8c1009963861
Samya brata Maji <samyabratamaji334@gmail.com>
2023-04-04 23:17:29 +0530

    added path aliases for vite (#171)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: 'actions',
+        replacement: path.resolve(__dirname, './src/actions'),
+      },
+      {
+        find: 'components',
+        replacement: path.resolve(__dirname, './src/components'),
+      },
+      {
+        find: 'Controls',
+        replacement: path.resolve(__dirname, './src/Controls'),
+      },
+      {
+        find: 'images',
+        replacement: path.resolve(__dirname, './src/images'),
+      },
+      {
+        find: 'utils',
+        replacement: path.resolve(__dirname, './src/utils'),
+      },
+    ],
+  },
+  plugins: [svgr(), react()],
 })
