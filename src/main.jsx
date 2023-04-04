7244e101ac7d8306be564c226f529a8d8078f706
Samya brata Maji <samyabratamaji334@gmail.com>
2023-04-04 15:45:54 +0530

    added: vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const container = document.getElementById('valhalla-app-root')
+render(
+  <React.StrictMode>
+    <h1>Valhalla FOSSGIS Server Demo App</h1>
+  </React.StrictMode>,
+  container
+)
